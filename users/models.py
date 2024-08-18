1e27b8bff2b50ef3e840194bdf1e4302e9dea399
air <talko.alexander@gmail.com>
2024-08-18 22:50:26 +0400

    подключена документация подключена возможность оплаты через stripe

--- users/models.py
+++ users/models.py
@@ -43,6 +43,9 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='сумма оплаты')
     payment_type = models.CharField(max_length=50, choices=PAYMENT_CHOICES, verbose_name='тип оплаты')
 
+    session_id = models.CharField(max_length=255, verbose_name='id сессии', **NULLABLE)
+    link = models.URLField(max_length=400, verbose_name='ссылка на оплату', **NULLABLE)
+
     class Meta:
         verbose_name = 'оплата'
         verbose_name_plural = 'оплаты'
--- users/views.py
+++ users/views.py
@@ -6,6 +6,8 @@ from users.serializers import PaymentSerializer, UserSerializer
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
+from users.services import create_stripe_price, create_stripe_session, create_stripe_product
+
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
@@ -24,3 +26,17 @@ class PaymentListAPIView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('lesson_paid', 'course_paid', 'payment_type',)
     ordering_fields = ('date_payment',)
+
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    def perform_create(self, serializer):
+        payment = serializer.save(user=self.request)
+        product_id = create_stripe_product(payment)
+        price_id = create_stripe_price(product_id, payment.amount)
+        session_id, payment_link = create_stripe_session(price_id)
+        payment.session_id = session_id
+        payment.link = payment_link
+        payment.save()
